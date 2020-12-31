a52f04c63361a79ad765278824babfd1fcf6a983
Chisato Murakami <kiwipom1031@gmail.com>
2020-12-30 16:12:35 -0800

    add example controller and views

---
+++ app/controllers/examples_controller.rb
@@ -0,0 +1,62 @@
+class ExamplesController < ApplicationController
+  before_action :set_example, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @examples = Example.all
+  end
+
+  def show
+  end
+
+  def new
+    @example = Example.new
+  end
+
+  def edit
+  end
+
+  def create
+    @example = Example.new(example_params)
+
+    respond_to do |format|
+      if @example.save
+        format.html { redirect_to @example, notice: 'Example was successfully created.' }
+        format.json { render :show, status: :created, location: @example }
+      else
+        format.html { render :new }
+        format.json { render json: @example.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @example.update(example_params)
+        format.html { redirect_to @example, notice: 'Example was successfully updated.' }
+        format.json { render :show, status: :ok, location: @example }
+      else
+        format.html { render :edit }
+        format.json { render json: @example.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @example.destroy
+    respond_to do |format|
+      format.html { redirect_to examples_url, notice: 'Example was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_example
+      @example = Example.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def example_params
+      params.require(:example).permit(:language, :sentence1, :sentence2, :clip_name, :grammar_id)
+    end
+end
