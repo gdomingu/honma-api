0a879689d7432faf3a1e64fb68b12b55b7d987ed
Chisato Murakami <kiwipom1031@gmail.com>
2021-03-08 14:22:55 -0800

    Added config and route for devise token.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  acts_as_token_authentication_handler_for User
+  protect_from_forgery with: :null_session, if: :json_request?
+
+  protected
+
+  def json_request?
+    request.format.json?
+  end
 end
