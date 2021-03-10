27b25133c3ec5f82ef69a2ee4e9064271c5e51d5
Chisato Murakami <kiwipom1031@gmail.com>
2021-03-10 11:11:55 -0800

    Added code to to prevent CSRF token.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   acts_as_token_authentication_handler_for User
   protect_from_forgery with: :null_session, if: :json_request?
+  skip_before_action :verify_authenticity_token 
+  respond_to :html, :json
 
   protected
 
