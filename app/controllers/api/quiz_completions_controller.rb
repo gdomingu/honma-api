0e272b48533f258259fe91c19c6f1aa4358ec6c7
Chisato Murakami <kiwipom1031@gmail.com>
2021-03-11 10:42:46 -0800

    Fixed querying methods when creating quiz_completion.

--- app/controllers/api/quiz_completions_controller.rb
+++ app/controllers/api/quiz_completions_controller.rb
@@ -2,8 +2,8 @@ module Api
   class QuizCompletionsController < ApplicationController
     def create
       token = params["authentication_token"]
-      user = User.where(authentication_token: token).first
-      quiz_completion = QuizCompletion.create!(user_id: user.id, quiz_id: params["quiz_id"])
+      user = User.find_by(authentication_token: token)
+      quiz_completion = QuizCompletion.find_or_create_by(user_id: user.id, quiz_id: params["quiz_id"])
       json_response(quiz_completion)
     end
 
