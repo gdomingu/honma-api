8dc644a73d56b1cbec2214d070da9530b5761a50
Chisato Murakami <kiwipom1031@gmail.com>
2021-10-23 19:33:13 -0700

    added Geocode gem and show address

--- app/controllers/place_infos_controller.rb
+++ app/controllers/place_infos_controller.rb
@@ -6,5 +6,8 @@ class PlaceInfosController < ApplicationController
   
   def show
     @place_info = PlaceInfo.find(params[:id])
+    latitude = @place_info.latitude
+    longtitude = @place_info.longtitude
+    @address = Geocoder.search([latitude, longtitude]).first.address
   end
 end
