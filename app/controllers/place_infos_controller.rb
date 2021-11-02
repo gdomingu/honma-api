ef052dc872098ce334e060c49070512a2fae73c9
Chisato Murakami <kiwipom1031@gmail.com>
2021-11-01 21:59:32 -0700

    add set_place_info

--- app/controllers/place_infos_controller.rb
+++ app/controllers/place_infos_controller.rb
@@ -1,11 +1,12 @@
 class PlaceInfosController < ApplicationController
 
+  before_action :set_place_info, only: [:show, :edit, :update, :destroy]
+
   def index
     @place_infos = PlaceInfo.all
   end
   
   def show
-    @place_info = PlaceInfo.find(params[:id])
     if @place_info.latitude.present? && @place_info.longtitude.present?
       latitude = @place_info.latitude
       longtitude = @place_info.longtitude
@@ -39,12 +40,15 @@ class PlaceInfosController < ApplicationController
   end
 
   def destroy
-    @place_info = PlaceInfo.find(params[:id])
     @place_info.destroy
     redirect_to place_infos_url, notice: 'Place info was successfully destroyed.'
   end
 
   private
+    def set_place_info
+      @place_info = PlaceInfo.find(params[:id])
+    end
+
     def place_info_params
       params.require(:place_info).permit(:name_en, :name_jp, :img_url, :dialect_id, :latitude, :longtitude)
     end
