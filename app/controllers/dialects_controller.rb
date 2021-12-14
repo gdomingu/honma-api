341973741c88a6f800ad2b043fb0ae94c0a1b37f
Chisato Murakami <kiwipom1031@gmail.com>
2021-12-13 20:42:46 -0800

    crud description for dialect and place_info

--- app/controllers/dialects_controller.rb
+++ app/controllers/dialects_controller.rb
@@ -69,6 +69,6 @@ class DialectsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def dialect_params
-      params.require(:dialect).permit(:name_en, :name_jp, :start_btn_text, :next_btn_text, :complete_btn_text, :default_image)
+      params.require(:dialect).permit(:name_en, :name_jp, :start_btn_text, :next_btn_text, :complete_btn_text, :description, :default_image)
     end
 end
