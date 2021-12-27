e5625ad1522fc92d627b63b712cb206bdd3ef977
Chisato Murakami <kiwipom1031@gmail.com>
2021-12-27 13:35:42 -0800

    sort by area from north to south

--- app/models/dialect.rb
+++ app/models/dialect.rb
@@ -3,5 +3,5 @@ class Dialect < ApplicationRecord
   has_many :place_infos
   has_one_attached :default_image
 
-  enum status: [:hokkaido, :tohoku, :kanto, :chubu, :kinki, :chugoku, :shikoku, :kyushu, :not_set]
+  enum areas: { Hokkaido: 0, Tohoku: 1, Kanto: 2, Chubu:3, Kinki: 4, Chugoku: 5,Shikoku: 6, Kyushu: 7, Not_set: 8 }
 end
