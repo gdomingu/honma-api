5dcd95cb92a42641d78f35e09bcc3eae78c9f0e3
Chisato Murakami <kiwipom1031@gmail.com>
2023-08-31 14:45:26 -0700

    fix tests

--- spec/controllers/dialects_controller_spec.rb
+++ spec/controllers/dialects_controller_spec.rb
@@ -1,13 +1,22 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
+require 'faker'
 
 RSpec.describe DialectsController, type: :controller do
-  describe "GET /index" do
+  describe 'GET /index' do
     before do
-      sign_in(User.create(email: 'rspectest@test.com', admin: false, password: 'password'))
+      sign_in(
+        User.create(
+          email: Faker::Internet.email,
+          admin: false,
+          password: Faker::Internet.password
+        )
+      )
       get :index, format: :json
     end
 
-    it "returns all dialects" do
+    it 'returns all dialects' do
       expect(response.status).to eq(200)
     end
   end
