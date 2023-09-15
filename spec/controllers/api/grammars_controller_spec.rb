4877883aa06ad3cb106c8b7d2d387bf2fe20abb2
Chisato Murakami <kiwipom1031@gmail.com>
2023-09-15 16:07:24 -0700

    create grammars controller

---
+++ spec/controllers/api/grammars_controller_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe Api::GrammarsController, type: :controller do
+  describe '#index' do
+    before do
+      sign_in(
+        User.create(
+          email: Faker::Internet.email,
+          admin: false,
+          password: Faker::Internet.password
+        )
+      )
+    end
+
+    it 'returns 200' do
+      get :index
+      expect(response).to be_successful
+    end
+  end
+end
