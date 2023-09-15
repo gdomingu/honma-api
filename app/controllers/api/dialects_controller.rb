8c7d11206bb89ef07e23bcf51edf8fe94d6f6151
Chisato Murakami <kiwipom1031@gmail.com>
2023-09-15 16:02:56 -0700

    optimize api dialects_controller

--- app/controllers/api/dialects_controller.rb
+++ app/controllers/api/dialects_controller.rb
@@ -1,42 +1,24 @@
-module Api 
+# frozen_string_literal: true
+
+module Api
   class DialectsController < ApplicationController
     def index
-      dialects = Dialect.order(:name_en).includes(grammars: [:examples, :quizzes]).map do |dialect| 
-        { 
-          id: dialect.id,
-          name_en: dialect.name_en, 
-          name_jp: dialect.name_jp, 
-          start_btn_text: dialect.start_btn_text, 
-          next_btn_text: dialect.next_btn_text, 
-          complete_btn_text: dialect.complete_btn_text, 
-          description: dialect.description,
-          area: Dialect.areas.key(dialect.area),
-          default_image: url_for(dialect.default_image),
-          grammars: dialect.grammars.order(:position).map do |grammar|
-             {
-              id: grammar.id,
-              label: grammar.label,
-              description: grammar.description,
-              position: grammar.position,
-              commonness: grammar.commonness,
-              examples: grammar.examples.map {|example|
-                url = example.audio_clip.present? ? url_for(example.audio_clip) : nil
-                example.attributes.merge!(audio_clip_url: url)
-              },
-              quizzes: grammar.quizzes.map {|quiz|
-                {
-                  id: quiz.id, 
-                  tokyo: quiz.tokyo,  
-                  answer: quiz.answer,
-                  quiz_completed: QuizCompletion.find_by(user: current_user, quiz: quiz)&.created_at
-                }
-              }
-            }
-          end,
-        }  
-      end
+      dialects =
+        Dialect.order(:name_en).includes([default_image_attachment: :blob]).map do |dialect|
+          {
+            id: dialect.id,
+            name_en: dialect.name_en,
+            name_jp: dialect.name_jp,
+            start_btn_text: dialect.start_btn_text,
+            next_btn_text: dialect.next_btn_text,
+            complete_btn_text: dialect.complete_btn_text,
+            description: dialect.description,
+            area: Dialect.areas.key(dialect.area),
+            default_image: dialect.default_image.present? ? url_for(dialect.default_image) : nil
+          }
+        end
+
       render json: dialects
     end
   end
 end
-
